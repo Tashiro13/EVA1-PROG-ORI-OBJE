3973b3435d3912c95ff045b6f6ee7adf4a23e682
Tashiro13 <143568865+Tashiro13@users.noreply.github.com>
2024-02-06 18:22:42 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_clases_objetos;
+
+/**
+ *
+ * @author alex_
+ */
+public class EVA1_1_CLASES_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona perso1;
+        int valor;
+    }
+    
+}
+
+
+class Persona
